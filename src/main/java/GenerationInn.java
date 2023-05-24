5777fcc6fb803019ab08eeb465caaa4a0d3117fa
Няшин Александр Владимирович <Nyashin-AV@ao-rr.rrgit config --global user.email Nyashin-AV@ao-rr.rr>
2023-05-24 17:31:29 +0300

    fix

---
+++ src/main/java/GenerationInn.java
@@ -0,0 +1,29 @@
+import com.github.javafaker.Faker;
+
+public class GenerationInn {
+
+    Faker faker = new Faker();
+
+    public String generationNewInn() {
+
+        String[] code = {"7846", "7847", "7900", "7900", "7901"};
+
+        String randomCode = code[(int) Math.floor(Math.random() * code.length)];
+        String randomNum = String.valueOf(faker.number().numberBetween(100000, 999999));
+
+        int oneNumber = ((Integer.valueOf(String.valueOf(randomCode.charAt(0))) * 7
+                + Integer.valueOf(String.valueOf(randomCode.charAt(1))) * 2 + Integer.valueOf(String.valueOf(randomCode.charAt(2))) * 4
+                + Integer.valueOf(String.valueOf(randomCode.charAt(3))) * 10 + Integer.valueOf(String.valueOf(randomNum.charAt(0))) * 3 + Integer.valueOf(String.valueOf(randomNum.charAt(1))) * 5
+                + Integer.valueOf(String.valueOf(randomNum.charAt(2))) * 9 + Integer.valueOf(String.valueOf(randomNum.charAt(3))) * 4 + Integer.valueOf(String.valueOf(randomNum.charAt(4))) * 6
+                + Integer.valueOf(String.valueOf(randomNum.charAt(5))) * 8)) % 11 % 10;
+
+        int twoNumber = ((Integer.valueOf(String.valueOf(randomCode.charAt(0))) * 3 + Integer.valueOf(String.valueOf(randomCode.charAt(1))) * 7 + Integer.valueOf(String.valueOf(randomCode.charAt(2))) * 2
+                + Integer.valueOf(String.valueOf(randomCode.charAt(3))) * 4 + Integer.valueOf(String.valueOf(randomNum.charAt(0))) * 10 + Integer.valueOf(String.valueOf(randomNum.charAt(1))) * 3
+                + Integer.valueOf(String.valueOf(randomNum.charAt(2))) * 5 + Integer.valueOf(String.valueOf(randomNum.charAt(3))) * 9 + Integer.valueOf(String.valueOf(randomNum.charAt(4))) * 4
+                + Integer.valueOf(String.valueOf(randomNum.charAt(5))) * 6 + (oneNumber * 8))) % 11 % 10;
+
+        String inn = randomCode + randomNum + oneNumber + twoNumber;
+
+        return inn;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static GenerationInn inn = new GenerationInn();
     public static void main(String[] args) {
-        System.out.println("Hello world Стас");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Что нужно сделать? Введите цифру:");
+        System.out.println("1. Сгенерировать ИНН");
+        String num = scanner.nextLine();
+
+        if (num.equals("1")) {
+            System.out.println(inn.generationNewInn());
+        }
     }
 }
